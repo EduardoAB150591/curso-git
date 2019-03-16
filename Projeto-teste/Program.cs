1ad658b4a84546d11c0b0c203a27d8c71215d704
Eduardo Araujo Bezerra <araujo_edu1991@hotmail.com>
2019-03-15 23:14:08 -0300

    acrescentar mensagem desejando um otimo dia

--- Projeto-teste/Program.cs
+++ Projeto-teste/Program.cs
@@ -4,8 +4,8 @@ namespace Projeto_teste {
     class Program {
         static void Main(string[] args) {
             Console.WriteLine("Hello World!");
-            Console.WriteLine("bom dia");
             Console.WriteLine("Como voce esta hoje?");
+            Console.WriteLine("tenha um excelente bom dia");
         }
     }
 }
